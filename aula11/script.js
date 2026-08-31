74b5445fa4b0134e5e0e94e3b55269fd2b19daa6
Seu Nome <seu.email@example.com>
2026-08-31 20:50:15 -0300

    adiciona arquivos HTML e JavaScript iniciais

---
+++ aula11/script.js
@@ -0,0 +1,7 @@
+
+
+const num1 = parseFloat(prompt("Digite um número:"));
+const num2 = parseFloat(prompt("Digite outro número:"));
+
+const soma = num1 + num2;
+alert("A soma dos números é: " + soma); // exibe o resultado da soma na tela do navegador
