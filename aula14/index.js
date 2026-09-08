a1e307259b1c2f6e97efc7c734e2bf12db8df2ee
emimartins1 <emilymartins.invest@gmail.com>
2026-09-08 16:54:27 -0300

    adiciona exercícios de manipulação de strings e números em JavaScript

---
+++ aula14/index.js
@@ -0,0 +1,10 @@
+let num1 = 10;
+let num2 = 2.5;
+
+// console.log(num1.toString() + num2); concatenação 
+// console.log(num1.toString(2));  binario
+
+// console.log(num1.toFixed(2)); arredondado 
+
+// verificando se o numero é inteiro console.log(Number.isInteger(num1));
+
