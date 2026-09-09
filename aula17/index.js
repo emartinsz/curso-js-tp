a07820175912fceace1d77c111d6cd37dcd326a5
emimartins1 <emilymartins.invest@gmail.com>
2026-09-09 16:25:01 -0300

    adiciona função soma e exemplo de uso no arquivo index.js

---
+++ aula17/index.js
@@ -0,0 +1,13 @@
+/* function saudacao(nome) {
+  return `Bom dia ${nome}!`;
+}
+
+const variavel = saudacao("Emily");
+console.log(variavel) */ 
+
+function soma(x, y){
+    const resultdo = x + y;
+    return resultdo;
+}
+
+console.log(soma(2,2));
