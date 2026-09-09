f6740dd28d46957b092d07d8524aae04469d6bfd
emimartins1 <emilymartins.invest@gmail.com>
2026-09-09 15:19:55 -0300

    adiciona arquivo index.js com exemplos de manipulação de arrays

---
+++ aula16/index.js
@@ -0,0 +1,30 @@
+//            01234
+const nome = 'Emily'
+
+//                0        1       2        indice
+const alunos = ["Emily", "Ana", "Maria"];
+
+console.log(alunos);
+
+
+// delete alunos[1]; // remove algum indice
+
+//const removido = alunos.shift(); remove a primeira coisa da lista
+// const removido = alunos.pop(); // remove a ultima coisa da lista
+
+
+// alunos.unshift('Luiza') // adiciona no começo na primeira lista, ordem. 
+
+// alunos.push("Natasha"); // adiciona no fim 
+
+//alunos[alunos.length] = "Fernanda"; adiciona mais um nome a lista
+//alunos[alunos.length] = "Gabi";
+//alunos[alunos.length] = "Isadora";
+
+// alunos[3] = "Luiza"; // Adicionando mais um nome a array
+
+
+//alunos[0] = "Eduardo" trocando 
+
+//console.log(alunos[1])
+//console.log(alunos);
