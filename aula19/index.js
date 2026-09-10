15b03a2870c3215cc61312e5d555c8831aca3a64
emimartins1 <emilymartins.invest@gmail.com>
2026-09-10 15:51:26 -0300

    adiciona arquivo index.js com exemplos de tipos de dados primitivos e referências mutáveis

---
+++ aula19/index.js
@@ -0,0 +1,10 @@
+// tipos de dados primitivos = string, number , boolean, undefined, null (bigint, symboll) são imutaveis. 
+
+// referencia mutaveis = array, object e function
+
+/* let nome = 'Emily';
+console.log(nome[0]); */ 
+
+let a = "em";
+let b = a; // copia o valor de a
+console.log(a,b);
